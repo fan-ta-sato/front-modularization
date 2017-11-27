74e54af8c8319ef5196200665a6674fac9d14456
fan-ta-sato <ta_sato@fancs.com>
2017-11-27 20:43:06 +0900

    .babelrc作成とminify, es3化調整

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,54 @@
 const webpack = require("webpack");
+// const MinifyPlugin = require("babel-minify-webpack-plugin");
 const ES5to3OutputPlugin = require("es5to3-webpack-plugin");
 
 module.exports = {
-    // エントリーポイントの指定
-    entry: './src/main.js',
-    // ファイルの出力設定
-    output: {
-        // 出力先のフォルダー名
-        path: `${__dirname}/build`,
-        // 出力ファイル名
-        filename: 'bundle.js'
-    },
+  // エントリーポイントの指定
+  entry: './src/main.js',
+  // ファイルの出力設定
+  output: {
+    // 出力先のフォルダー名
+    path: `${__dirname}/build`,
+    // 出力ファイル名
+    filename: 'bundle.js'
+  },
 
-    module: {
-        rules: [
-        {
-            // 拡張子 .js の場合
-            test: /\.js$/,
-            use: [
-            {
-                // Babel を利用する
-                loader: 'babel-loader',
-                // Babel のオプションを指定する
-                options: {
-                    presets: [
-                        // env を指定することで、ES2017 を ES5 に変換。
-                        // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
-                        // webpack の Tree Shaking 機能が使えない
-                        ['env', {'modules': false}]
-                    ]
-                }
-            }
-            ]
-        }
+  module: {
+    rules: [
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: 'babel-loader',
+          }
         ]
-    },
-
-
-    // ソースマップを有効にする
-    devtool: 'source-map',
-
-    // ローカル開発用環境を立ち上げる
-    // ブラウザで http://localhost:8081/ でアクセスできるようになる
-    devServer: {
-        contentBase: 'build',
-        port: 8089
-    },
-
-    plugins: [
-        // // // JSファイルのminifyを実行する
-        new webpack.optimize.UglifyJsPlugin({
-            // minify時でもソースマップを利用する
-            sourceMap: true
-        }),
-
-        // // ES5 -> ES3
-        new ES5to3OutputPlugin(),
+      }
     ]
+  },
+
+
+  // ソースマップを有効にする
+  devtool: 'source-map',
+
+  // ローカル開発用環境を立ち上げる
+  // ブラウザで http://localhost:8081/ でアクセスできるようになる
+  devServer: {
+    contentBase: 'build',
+    port: 8089
+  },
+
+  plugins: [
+    // Babel公式
+    // エラーになる
+    // new MinifyPlugin()
+    // // JSファイルのminifyを実行する
+    // new webpack.optimize.UglifyJsPlugin({
+    //   // minify時でもソースマップを利用する
+    //   sourceMap: true
+    // }),
+    // // ES5 -> ES3
+    // new ES5to3OutputPlugin(),
+  ]
 };
